29fec130b87fd173c08bc4c17e0567eb902bc82e
Ubuntu <ubuntu@ip-172-31-74-87.ec2.internal>
2022-05-12 23:16:02 +0000

    fixed code

--- operations.py
+++ operations.py
@@ -1,48 +1,54 @@
-# input 
-def calculate(operation_input,number1,number2):
- 
-    
+# input
+def calculate(operator_input, number1, number2):
+
     # process
-    
-    # addition 
-    if operation_input == "+": 
+
+    # addition
+    if operator_input == "+":
         equation = number1 + number2
         return equation
-    # subtraction 
-    elif operation_input == "-":
-        equation = number1 - number2 
+    # subtraction
+    elif operator_input == "-":
+        equation = number1 - number2
         return equation
-    # multiplcation 
-    elif operation_input == "*":
-        equation = number1 * number2 
-        return equation 
-    # divison 
-    elif operation_input == "/":
-        equation = number1 / number2 
-        return equation 
+    # multiplcation
+    elif operator_input == "*":
+        equation = number1 * number2
+        return equation
+    # divison
+    elif operator_input == "/":
+        equation = number1 / number2
+        return equation
     # remainder
-    elif operation_input == "%":
-        equation = number1 % number2 
-        return equation 
-    else: 
-        print("invalid operation, enter a operation")
+    elif operator_input == "%":
+        equation = number1 % number2
+        return equation
+    else:
+        equation = "nothing"
+        return equation
+
+
 def main():
-    # values 
+    # values
     number1 = 0
-    number2 = 0 
-    # input 
-    try: 
-        operation_input = input("enter an operation: ")
-        number1 = int(input("enter a number: "))
-        number2 = int(input("enter a number again: ")) 
-    except ValueError: 
-        print("please enter a integer")
-    # call functions 
-    calculate(operation_input,number1,number2) 
-    
-    end = calculate(operation_input,number1,number2)
-    
-    print("the result of {} {} {} is {}".format(number1, operation_input,number2, end))
+    number2 = 0
+    # input
+    try:
+        operator_input = input("Enter an operator ex..'+' '%' '/' '-' '*' : ")
+        number1 = int(input("Enter the first number: "))
+        number2 = int(input("Enter the second number: "))
+    except ValueError:
+        print("Please enter a integer")
+    # call functions
+    calculate(operator_input, number1, number2)
+    # solution
+    sol = calculate(operator_input, number1, number2)
+    if sol != "nothing":
+        print("The result of {} {} {} is {}".format(
+            number1, operator_input, number2, sol))
+    elif sol == "nothing":
+        print("Enter a valid operator")
+
 
 if __name__ == "__main__":
-            main() 
+    main()
